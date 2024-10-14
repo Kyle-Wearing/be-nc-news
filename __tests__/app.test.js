12985a7f7e057afc0383986abf3166beee631349
Kyle-Wearing <kylewearing@icloud.com>
2024-10-14 15:33:11 +0100

    create the article by id end point with error handling for 404 and 400 errors

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,7 +5,7 @@ const seed = require("../db/seeds/seed");
 const request = require("supertest");
 const endpointsJSON = require("../endpoints.json");
 
-beforeAll(() => seed(data));
+beforeEach(() => seed(data));
 afterAll(() => db.end());
 
 describe("GET requests", () => {
@@ -30,6 +30,41 @@ describe("GET requests", () => {
             expect(typeof topic.slug).toBe("string");
             expect(typeof topic.description).toBe("string");
           });
+        });
+    });
+  });
+  describe("/api/articles/:rticle_id", () => {
+    it("responds 200 - returns an article object with all the article properties", () => {
+      return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then(({ body: { article } }) => {
+          expect(Object.keys(article[0]).length).toBe(8);
+          expect(article[0].title).toBe("Living in the shadow of a great man");
+          expect(article[0].topic).toBe("mitch");
+          expect(article[0].author).toBe("butter_bridge");
+          expect(article[0].body).toBe("I find this existence challenging");
+          expect(article[0].created_at).toBe("2020-07-09T20:11:00.000Z");
+          expect(article[0].votes).toBe(100);
+          expect(article[0].article_img_url).toBe(
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+          );
+        });
+    });
+    it("responds 404 - returns an error message if given a valid id that does not exist", () => {
+      return request(app)
+        .get("/api/articles/9999999")
+        .expect(404)
+        .then(({ body: { msg } }) => {
+          expect(msg).toBe("Article does not exist");
+        });
+    });
+    it("responds 400 - return an error message if given an invalid id type", () => {
+      return request(app)
+        .get("/api/articles/invalid_id")
+        .expect(400)
+        .then(({ body: { msg } }) => {
+          expect(msg).toBe("Invalid id type");
         });
     });
   });
