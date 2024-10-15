a9c79cbc7f74c8450ff236de9d2b007fe22d19c4
Kyle-Wearing <kylewearing@icloud.com>
2024-10-15 10:53:04 +0100

    update topics cintroller and model

--- models/topics.model.js
+++ models/topics.model.js
@@ -1,8 +1,14 @@
 const db = require("../db/connection");
 
 function selectTopics() {
-  return db.query(`
-    SELECT * FROM topics`);
+  return db
+    .query(
+      `
+    SELECT * FROM topics`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
 }
 
 module.exports = { selectTopics };
