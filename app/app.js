7f5e443f74d9ae471a42033721b78e06c2a29f31
Kyle-Wearing <kylewearing@icloud.com>
2024-10-18 14:23:04 +0100

    create routers for articles, topics, comments, users

--- app/app.js
+++ app/app.js
@@ -13,28 +13,13 @@ const {
   deleteCommentById,
 } = require("../controllers/comments.controller");
 const { getUsers } = require("../controllers/users.controller");
+const apiRouter = require("../routers/api.router");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/api", getEndpoints);
-
-app.get("/api/topics", getTopics);
-
-app.get("/api/users", getUsers);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-
-app.post("/api/articles/:article_id/comments", postCommentByArticleId);
-
-app.patch("/api/articles/:article_id", patchArticleById);
-
-app.delete("/api/comments/:comment_id", deleteCommentById);
+app.use("/api", apiRouter);
 
 app.use(customErrors);
 app.use(sqlErrors);
